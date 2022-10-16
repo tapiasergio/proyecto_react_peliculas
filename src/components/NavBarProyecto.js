ab0661e283c43d20000899ce8b8e7ecd7724114f
tapiasergio <sergiotapia782@gmail.com>
2022-10-16 06:32:02 -0300

    Second Commit
    NavBar mejorado

--- src/components/NavBarProyecto.js
+++ src/components/NavBarProyecto.js
@@ -1,26 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
+import { NavDropdown, Offcanvas, OffcanvasBody, OffcanvasHeader, OffcanvasTitle } from "react-bootstrap";
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { Outlet,Link } from "react-router-dom";
+import InfoLogo from './Images/infoIcon.png'
+
 
 const NavBarProyecto = () => {
-    
+        const [show,setShow] = useState(false);
+
+        const handleClose = () => setShow(false);
+        const handleShow = () => setShow(true);
         return(
 
             <>
-            <Navbar bg="light" expand="lg">
+            <Navbar  bg="primary" variant="dark" expand="lg">
         <Container>
-            <Navbar.Brand as={Link} to = "/">Cine Tapia</Navbar.Brand>
+            <Navbar.Brand as={Link} to = "/">CineTapia</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-                <Nav.Link as={Link} to = "/">Inicio</Nav.Link>
-                <Nav.Link as={Link} to = "/Peliculas">Peliculas</Nav.Link>
-                <Nav.Link as={Link} to = "/Principal">Principal</Nav.Link>
-                
+                <Nav.Link className="px-auto" as={Link} to = "/Peliculas">Peliculas</Nav.Link>
+                <Nav.Link className="ms-1" as={Link} to = "/Principal">Estrenos</Nav.Link>
+                <NavDropdown title="Categorias" id="basic-nav-dropdown">
+                    <NavDropdown.Item as={Link} to = "/Peliculas">Accion</NavDropdown.Item>
+                    <NavDropdown.Divider/>
+                    <NavDropdown.Item as={Link} to = "/Peliculas">Ver Más</NavDropdown.Item>
+                </NavDropdown>
+                <NavDropdown title="Más" id="basic-nav-dropdown">
+                    <NavDropdown.Item as={Link} to = "/Peliculas">Accion</NavDropdown.Item>
+                    <NavDropdown.Divider/>
+                    <NavDropdown.Item as={Link} to = "/Peliculas">Ver Más</NavDropdown.Item>
+                </NavDropdown>
             </Nav>
+            <Nav>
+            <button className="boton-info" onClick={handleShow}>
+                <img className="iLogo" src={InfoLogo} alt="info"></img>
+            </button>
+            <Offcanvas show={show} onHide={handleClose} placement={'end'}>
+                <OffcanvasHeader closeButton>
+                    <OffcanvasTitle>Información <img className="iLogo" src={InfoLogo} alt="info"></img></OffcanvasTitle>
+                </OffcanvasHeader>
+                <OffcanvasBody>
+                    <div className="info-canvas">
+                    <p>Esta pagina es creada para la materia Programación Web.</p>
+                    <p>Es una pagina web de un Cine Ficticio.</p>
+                    <p>Se puede salir de este canvas haciendo click fuera de él.</p>
+                    <p>Se pueden encontrar distintas cosas creadas por mi en Ver Más.</p>
+                    <hr></hr>
+                    <h4>Creada por Tapia Sergio.</h4>
+                    <a href="https://github.com/tapiasergio" >Ir a mi Repositorio</a>
+                    </div>
+                    
+                </OffcanvasBody>
+            </Offcanvas>
+            </Nav>
+            
             </Navbar.Collapse>
+            
         </Container>
     </Navbar>
             <section>
